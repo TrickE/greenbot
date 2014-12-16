094d3ac320827d5be6cebe77ebf6d50ec18e2829
TrickE <f3g60@notsharingmy.info>
2014-12-17 12:06:59 +1300

    Revert "Base Code"
    This reverts commit 2fee7b7b0da2717d1865271d875f73cb361b5c35.

--- greenbot/Form1.vb
+++ greenbot/Form1.vb
@@ -1,23 +1,3 @@
-﻿Imports greenbot.Mouse
-Imports greenbot.Keyboard
-Imports greenbot.bot
-Public Class Form1
+﻿Public Class Form1
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Mouse.mouseSpeed = 8
-        Mouse.MoveMouse(New Point(50, 90))
-        waitms(1)
-        Mouse.mouseSpeed = 10
-        Mouse.MoveMouse(New Point(90, 220))
-        wait(1)
-        Mouse.mouseSpeed = 20
-        Mouse.MoveMouse(New Point(900, 1000))
-        wait(0.3)
-        Mouse.mouseSpeed = 8
-        Mouse.MoveMouse(New Point(900, 900))
-    End Sub
 End Class
